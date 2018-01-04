e7b0226f17ff8df15c36cf40dc6ed5baf5ea7b5f
PronabRoy <35110028+PronabRoy@users.noreply.github.com>
2018-01-05 01:23:33 +0600

    Update main.cpp

--- Cline/Cline/main.cpp
+++ Cline/Cline/main.cpp
@@ -5,8 +5,13 @@ int main()
 
 	std::string ip = "127.0.0.1";
 	int port = 1111;
-
-	std::cin >> ip;
+	
+	std::cout << "To Download any File form Server 1st Wtite -> File then enter" << std::endl;
+	std::cout << "Then Write file full name then enter" << std::endl;
+	std::cout << "For Example : File (Enter) C++.png" << std::endl;
+	std::cout << "See Downloaded File in Cline\file folder\n" << std::endl;
+	std::cout << "To Open Camera in Server side write -> cam" << std::endl;
+	std::cout << "Press Ecs to close camera\n" << std::endl;
 
 	Client client(ip, port);
 	if (client.connectServer() == true)
